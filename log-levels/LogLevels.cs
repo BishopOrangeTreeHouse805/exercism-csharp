d8e753faee804daccb16553b87df53e6e197a8e1
John Paul Naranjo <swade84@hotmail.com>
2025-08-06 12:23:57 -0700

    Change target framework to .Net 8.0

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -1,11 +1,12 @@
 static class LogLine
 {
-    public static string Message(string logLine)
+    public static string Message(string logLine)//logline is the string variable in which message is extracted
     {
         throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
     }
 
-    public static string LogLevel(string logLine)
+    public static string LogLevel(string logLine)//logline is the string variable in which loglevel is extracted
+    
     {
         throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
     }
