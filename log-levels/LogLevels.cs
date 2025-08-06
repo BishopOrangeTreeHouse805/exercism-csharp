b0a24cc4038a5c271d10c1d4de3da7b3775cbedf
John Paul Naranjo <swade84@hotmail.com>
2025-08-06 13:30:55 -0700

    Add new exercism challenge files

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -1,18 +1,8 @@
+
 static class LogLine
 {
-    public static string Message(string logLine)//logline is the string variable in which message is extracted
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
-    }
-
-    public static string LogLevel(string logLine)//logline is the string variable in which loglevel is extracted
+    public static string Message(string logLine) => logLine.Substring(logLine.IndexOf(":") + 1).Trim();
+    public static string LogLevel(string logLine) => logLine.Substring(logLine.IndexOf("[") + 1, logLine.IndexOf("]") - 1).ToLower();
+    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
     
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
-
-    public static string Reformat(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
-    }
 }
