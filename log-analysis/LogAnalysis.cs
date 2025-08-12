9a8eccef151401b4e476e96f7508ac9358996f83
John Paul Naranjo <swade84@hotmail.com>
2025-08-12 10:11:18 -0700

    Added SubstringAfter() extension method.

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,7 +1,13 @@
+using Microsoft.Win32;
+
 public static class LogAnalysis 
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string str)
+    {
+        return str[(str.IndexOf(":") + 1)..].Trim();
+    }
 
+    
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     
     // TODO: define the 'Message()' extension method on the `string` type
