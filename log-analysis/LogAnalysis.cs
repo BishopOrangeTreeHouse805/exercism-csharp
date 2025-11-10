d8d480c823d4e98f5095745523eafa1730682772
John Paul Naranjo <swade84@hotmail.com>
2025-11-10 08:11:50 -0800

    Refined LogAnalysis logic and updated dependencies

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,16 +1,13 @@
+using System.Runtime.InteropServices;
 using Microsoft.Win32;
+
 
 public static class LogAnalysis 
 {
-    public static string SubstringAfter(this string str)
-    {
-        return str[(str.IndexOf(":") + 1)..].Trim();
-    }
-
-    
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
-
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string SubstringAfter(this string str, string delimiter)=> str[(str.IndexOf(delimiter) + 1)..].Trim();
+    public static string SubstringBetween(this string str, string delimiter1, string delimiter2)=> str[(str.IndexOf(delimiter1) + 1)..str.IndexOf(delimiter2)];
+    public static string Message(this string str) => str.SubstringAfter(":");
+    public static string LogLevel(this string str) => str.SubstringBetween("[", "]");
 }
+
+
